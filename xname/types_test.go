98a103da0383117cd84d291a31757704ae8eb6e4
Ryan Sjostrand <sjostrand@hpe.com>
2021-12-01 15:46:37 -0600

    Add tests for Parent and Children functions

--- xname/types_test.go
+++ xname/types_test.go
@@ -2,6 +2,7 @@ package xname
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 	"testing"
 
@@ -298,5 +299,513 @@ func TestToFromXnames(t *testing.T) {
 		if expectedHMSType != generatedXnameType {
 			t.Errorf("Unexpected generated xname %s (%s), expected (%s) %s", generatedXnameType, generatedXname, expectedHMSType, xname)
 		}
+	}
+}
+
+func TestSystemChildren(t *testing.T) {
+	system := System{}
+
+	// Create a child CDU
+	cdu := system.CDU(1)
+	expectedCDU := CDU{
+		CoolingGroup: 1,
+	}
+	if !reflect.DeepEqual(expectedCDU, cdu) {
+		t.Errorf("TestSystemChildren FAIL: Expected cdu=%v but instead got cdu=%v", expectedCDU, cdu)
+	}
+
+	// Create a child cabinet
+	cabinet := system.Cabinet(1)
+	expectedCabinet := Cabinet{
+		Cabinet: 1,
+	}
+	if !reflect.DeepEqual(expectedCabinet, cabinet) {
+		t.Errorf("TestSystemChildren FAIL: Expected cabinet=%v but instead got cabinet=%v", expectedCabinet, cabinet)
+	}
+}
+
+func TestSystemParent(t *testing.T) {
+	// A System doesn't have a parent
+}
+
+func TestCDUChildren(t *testing.T) {
+	cdu := CDU{
+		CoolingGroup: 1,
+	}
+
+	// Create a child CDUMgmtSwitch
+	cduMgmtSwitch := cdu.CDUMgmtSwitch(2)
+	expectedCDUMgmtSwitch := CDUMgmtSwitch{
+		CoolingGroup: 1,
+		Slot: 2,
+	}
+	if !reflect.DeepEqual(expectedCDUMgmtSwitch, cduMgmtSwitch) {
+		t.Errorf("TestCDUChildren FAIL: Expected cduMgmtSwitch=%v but instead got cduMgmtSwitch=%v", expectedCDUMgmtSwitch, cduMgmtSwitch)
+	}
+}
+
+func TestCDUParent(t *testing.T) {
+	cdu := CDU{
+		CoolingGroup: 1,
+	}
+
+	parent := cdu.Parent()
+	expectedParent := System{}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestCDUParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestCabinetChildren(t *testing.T) {
+	cabinet := Cabinet{
+		Cabinet: 1,
+	}
+
+	// Create a child CabinetPDUController
+	cabinetPDUController := cabinet.CabinetPDUController(2)
+	expectedCabinetPDUController := CabinetPDUController{
+		Cabinet: 1,
+		PDUController: 2,
+	}
+	if !reflect.DeepEqual(expectedCabinetPDUController, cabinetPDUController) {
+		t.Errorf("TestCabinetChildren FAIL: Expected cabinetPDUController=%v but instead got cabinetPDUController=%v", expectedCabinetPDUController, cabinetPDUController)
+	}
+
+	// Create a child Chassis
+	chassis := cabinet.Chassis(2)
+	expectedChassis := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedChassis, chassis) {
+		t.Errorf("TestCabinetChildren FAIL: Expected chassis=%v but instead got chassis=%v", expectedChassis, chassis)
+	}
+}
+func TestCabinetParent(t *testing.T) {
+	cabinet := Cabinet{
+		Cabinet: 1,
+	}
+	
+	parent := cabinet.Parent()
+	expectedParent := System{}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestCabinetParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestCabinetPDUControllerChildren(t *testing.T) {
+	// TODO no children structures have bene defined yet, but child xname formats have been defined
+}
+func TestCabinetPDUControllerParent(t *testing.T) {
+	cabinetPDUController := CabinetPDUController{
+		Cabinet: 1,
+		PDUController: 2,
+	}
+	
+	parent := cabinetPDUController.Parent()
+	expectedParent := Cabinet{
+		Cabinet: 1,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestCabinetPDUControllerParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestChassisChildren(t *testing.T) {
+	chassis := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+
+	// Create a child ComputeModule
+	computeModule := chassis.ComputeModule(3)
+	expectedComputeModule := ComputeModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedComputeModule, computeModule) {
+		t.Errorf("TestChassisChildren FAIL: Expected computeModule=%v but instead got computeModule=%v", expectedComputeModule, computeModule)
+	}
+
+	// Create a child NodeBMC
+	nodeBMC := chassis.NodeBMC(3, 4)
+	expectedNodeBMC := NodeBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	if !reflect.DeepEqual(expectedNodeBMC, nodeBMC) {
+		t.Errorf("TestChassisChildren FAIL: Expected nodeBMC=%v but instead got nodeBMC=%v", expectedNodeBMC, nodeBMC)
+	}
+
+	// Create a child MgmtSwitch
+	mgmtSwitch := chassis.MgmtSwitch(3)
+	expectedMgmtSwitch := MgmtSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedMgmtSwitch, mgmtSwitch) {
+		t.Errorf("TestChassisChildren FAIL: Expected mgmtSwitch=%v but instead got mgmtSwitch=%v", expectedMgmtSwitch, mgmtSwitch)
+	}
+
+	// Create a child MgmtHLSwitchEnclosure
+	mgmtHLSwitchEnclosure := chassis.MgmtHLSwitchEnclosure(3)
+	expectedMgmtHLSwitchEnclosure := MgmtHLSwitchEnclosure{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedMgmtHLSwitchEnclosure, mgmtHLSwitchEnclosure) {
+		t.Errorf("TestChassisChildren FAIL: Expected mgmtHLSwitchEnclosure=%v but instead got mgmtHLSwitchEnclosure=%v", expectedMgmtHLSwitchEnclosure, mgmtHLSwitchEnclosure)
+	}
+
+	// Create a child MgmtHLSwitch
+	mgmtHLSwitch := chassis.MgmtHLSwitch(3, 4)
+	expectedMgmtHLSwitch := MgmtHLSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		Space: 4,
+	}
+	if !reflect.DeepEqual(expectedMgmtHLSwitch, mgmtHLSwitch) {
+		t.Errorf("TestChassisChildren FAIL: Expected mgmtHLSwitch=%v but instead got mgmtHLSwitch=%v", expectedMgmtHLSwitch, mgmtHLSwitch)
+	}
+
+	// Create a child RouterModule
+	routerModule := chassis.RouterModule(3)
+	expetedRouterModule := RouterModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expetedRouterModule, routerModule) {
+		t.Errorf("TestChassisChildren FAIL: Expected routerModule=%v but instead got routerModule=%v", expetedRouterModule, routerModule)
+	}
+
+
+	// Create a child RouterBMC
+	routerBMC := chassis.RouterBMC(3, 4)
+	expectedRouterBMC := RouterBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	if !reflect.DeepEqual(expectedRouterBMC, routerBMC) {
+		t.Errorf("TestChassisChildren FAIL: Expected routerBMC=%v but instead got routerBMC=%v", expectedRouterBMC, routerBMC)
+	}
+
+}
+func TestChassisParent(t *testing.T) {
+	chassis := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	
+	parent := chassis.Parent()
+	expectedParent := Cabinet{
+		Cabinet: 1,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestChassisParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestChassisBMCChildren(t *testing.T) {
+	// TODO no children structures have bene defined yet, but child xname formats have been defined
+}
+func TestChassisBMCParent(t *testing.T) {
+	chassisBMC := ChassisBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		BMC: 0,
+	}
+	
+	parent := chassisBMC.Parent()
+	expectedParent := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestChassisBMCParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestMgmtSwitchChildren(t *testing.T) {
+	mgmtSwitch := MgmtSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+
+	// Create a child MgmtSwitchConnector
+	mgmtSwitchConnector := mgmtSwitch.MgmtSwitchConnector(4)
+	expectedMgmtSwitchConnector := MgmtSwitchConnector{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		SwitchPort: 4,
+	}
+	if !reflect.DeepEqual(expectedMgmtSwitchConnector, mgmtSwitchConnector) {
+		t.Errorf("TestMgmtSwitchChildren FAIL: Expected mgmtSwitchConnector=%v but instead got mgmtSwitchConnector=%v", expectedMgmtSwitchConnector, mgmtSwitchConnector)
+	}
+}
+func TestMgmtSwitchParent(t *testing.T) {
+	mgmtSwitch := MgmtSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	
+	parent := mgmtSwitch.Parent()
+	expectedParent := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestMgmtSwitchParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestMgmtSwitchConnectorChildren(t *testing.T) {
+	// There are no childlen for a MgmtSwitchConnector
+}
+func TestMgmtSwitchConnectorParent(t *testing.T) {
+	mgmtSwitchConnector := MgmtSwitchConnector{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		SwitchPort: 4,
+	}
+	
+	parent := mgmtSwitchConnector.Parent()
+	expectedParent := MgmtSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestMgmtSwitchConnectorParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestMgmtHLSwitchEnclosureChildren(t *testing.T) {
+	mgmtHLSwitchEnclosure := MgmtHLSwitchEnclosure{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+
+	// Create a child MgmtHLSwitch
+	mgmtHLSwitch := mgmtHLSwitchEnclosure.MgmtHLSwitch(4)
+	expectedMgmtHLSwitch := MgmtHLSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		Space: 4,
+	}
+	if !reflect.DeepEqual(expectedMgmtHLSwitch, mgmtHLSwitch) {
+		t.Errorf("TestMgmtHLSwitchEnclosureChildren FAIL: Expected mgmtHLSwitch=%v but instead got mgmtHLSwitch=%v", expectedMgmtHLSwitch, mgmtHLSwitch)
+	}
+}
+func TestMgmtHLSwitchEnclosureParent(t *testing.T) {
+	mgmtHLSwitchEnclosure := MgmtHLSwitchEnclosure{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	
+	parent := mgmtHLSwitchEnclosure.Parent()
+	expectedParent := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestMgmtHLSwitchEnclosureParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestMgmtHLSwitchChildren(t *testing.T) {
+	// TODO no children structures have bene defined yet, and currently no child xname formats have been defined
+}
+func TestMgmtHLSwitchParent(t *testing.T) {
+	mgmtHLSwitch := MgmtHLSwitch{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		Space: 4,
+	}
+	
+	parent := mgmtHLSwitch.Parent()
+	expectedParent := MgmtHLSwitchEnclosure{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestMgmtHLSwitchParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestRouterModuleChildren(t *testing.T) {
+	routerModule := RouterModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+
+	// Create a child RouterBMC
+	routerBMC := routerModule.RouterBMC(4)
+	expectedRouterBMC := RouterBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	if !reflect.DeepEqual(expectedRouterBMC, routerBMC) {
+		t.Errorf("TestRouterModuleChildren FAIL: Expected routerBMC=%v but instead got routerBMC=%v", expectedRouterBMC, routerBMC)
+	}
+}
+func TestRouterModuleParent(t *testing.T) {
+	routerModule := RouterModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	
+	parent := routerModule.Parent()
+	expectedParent := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestRouterModuleParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestRouterBMCChildren(t *testing.T) {
+	// TODO no children structures have bene defined yet, but child xname formats have been defined
+}
+func TestRouterBMCParent(t *testing.T) {
+	routerModule := RouterBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	
+	parent := routerModule.Parent()
+	expectedParent := RouterModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestRouterBMCParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestComputeModuleChildren(t *testing.T) {
+	computeModule := ComputeModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+
+	// Create a child NodeBMC
+	nodeBMC := computeModule.NodeBMC(4)
+	expectedNodeBMC := NodeBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+
+	if !reflect.DeepEqual(expectedNodeBMC, nodeBMC) {
+		t.Errorf("TestComputeModuleChildren FAIL: Expected nodeBMC=%v but instead got nodeBMC=%v", expectedNodeBMC, nodeBMC)
+	}
+}
+func TestComputeModuleParent(t *testing.T) {
+	computeModule := ComputeModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	
+	parent := computeModule.Parent()
+	expectedParent := Chassis{
+		Cabinet: 1,
+		Chassis: 2,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestComputeModuleParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+
+func TestNodeBMCChildren(t *testing.T) {
+	nodeBMC := NodeBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+
+	// Create a child Node
+	node := nodeBMC.Node(0)
+	expectedNode := Node{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+		Node: 0,
+	}
+	if !reflect.DeepEqual(expectedNode, node) {
+		t.Errorf("TestNodeBMCChildren FAIL: Expected node=%v but instead got node=%v", expectedNode, node)
+	}
+}
+
+func TestNodeBMCParent(t *testing.T) {
+	nodeBMC := NodeBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	
+	parent := nodeBMC.Parent()
+	expectedParent := ComputeModule{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestNodeBMCParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
+	}
+}
+func TestNodeChildren(t *testing.T) {
+	// TODO no children structures have bene defined yet, but child xname formats have been defined
+}
+
+func TestNodeParent(t *testing.T) {
+	node := Node{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+		Node: 0,
+	}
+	
+	parent := node.Parent()
+	expectedParent := NodeBMC{
+		Cabinet: 1,
+		Chassis: 2,
+		Slot: 3,
+		BMC: 4,
+	}
+	if !reflect.DeepEqual(expectedParent, parent) {
+		t.Errorf("TestNodeParent FAIL: Expected parent=%v but instead got parent=%v", expectedParent, parent)
 	}
 }
